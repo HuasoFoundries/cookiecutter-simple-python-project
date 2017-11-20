3d55c44c5ac38217b61fd0896a6be6bb9db75fcd
Gcantergiani <gonzalezcantergiani@gmail.com>
2017-11-20 16:36:30 -0300

    add dataframe to table method

--- {{cookiecutter.directory_name}}/src/db/load.py
+++ {{cookiecutter.directory_name}}/src/db/load.py
@@ -2,6 +2,7 @@ import psycopg2
 import psycopg2.extras
 
 from . import queries as queries
+from io import StringIO
 
 
 def load_data(data, url):
@@ -16,3 +17,17 @@ def load_data(data, url):
             data,
             template=None, page_size=100
         )
+
+
+def load_data_pd(df, url):
+    TABLE_OUTPUT = ''
+
+    with psycopg2.connect(url) as conn:
+        conn.autocommit = True
+
+        cursor = conn.cursor()
+        f = StringIO(df.to_csv(index=False,
+                               header=False,
+                               sep=';'))
+        cursor.copy_from(f, TABLE_OUTPUT,
+                         sep=';', null='')
