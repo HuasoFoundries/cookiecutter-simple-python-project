aef5bc147e169d96fbc9685918d9d831243ca453
Gcantergiani <gonzalezcantergiani@gmail.com>
2017-12-11 18:10:27 -0300

    Add __init__ to db directory - remove data inside init file

--- {{cookiecutter.directory_name}}/src/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for fb-custom-categories."""
-
-__author__ = """{{cookiecutter.author}}"""
-__email__ = '{{cookiecutter.email}}'
-__version__ = '{{cookiecutter.version}}'
